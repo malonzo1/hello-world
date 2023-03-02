dd2768c895d8e84c2dfc4214a428a95725020e3a
malonzo1 <manuelluisalonzo2014@gmail.com>
2023-03-02 08:06:42 +0000

    first build

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+from django.views.generic.base import RedirectView
+
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='admin/')),
     path('admin/', admin.site.urls),
     path('polls/', include('polls.urls')),
     
